5b8dfccd8dc1c042c8fadc96b2688f975300dfa5
Megan Olson <megan@>
2017-08-11 12:51:20 -0700

    Add photo to results

--- js/scripts.js
+++ js/scripts.js
@@ -15,9 +15,9 @@ $(document).ready(function() {
     else {
       if (answer1 === 'small companies and startups' && answer2 === 'yes - i love it' || answer2 === "no - I wouldn't know where to start" && answer3 === 'creating a formula to act with user input' && answer4 === 'no - I am persistent and determined but I like to see my project up and running with faster results' && answer5 === 'not neccesarily') {
         $('.ruby').show();
-      } else if (answer1 === 'large corporations' && answer2 === 'yes - i love it' || answer2 === "no - I wouldn't know where to start" && answer3 === 'writing elaborate software for a big database' && answer4 === 'yes - whatever it takes!' && answer5 ==='yes, please') {
+      } else if (answer1 === 'large corporations' && answer2 === 'yes - i love it' || answer2 === "no - I wouldn't know where to start" && answer3 === 'writing elaborate software for a big database' && answer4 === 'yes - whatever it takes!' && answer5 ==='definitely') {
         $('.c').show();
-      } else if (answer1 === 'large corporations' || answer1 === 'small companies and startups' && answer2 === 'yes - i love it' && answer3 === 'formatting a webpage according to user preference and usability' && answer4 === 'no - I am persistent and determined but I like to see my project up and running with faster results' || answer4 === 'yes - whatever it takes!' && answer5 ==='not neccesarily' || answer5 === 'yes, please') {
+      } else if (answer1 === 'large corporations' || answer1 === 'small companies and startups' && answer2 === 'yes - i love it' && answer3 === 'formatting a webpage according to user preference and usability' && answer4 === 'no - I am persistent and determined but I like to see my project up and running with faster results' || answer4 === 'yes - whatever it takes!' && answer5 ==='not neccesarily' || answer5 === 'definitely') {
         $('.css').show();
       } else {
         $('.leftover').show();
