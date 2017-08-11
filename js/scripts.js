17ffa18e4c96fcf332be8d6a0abfe0a2e9ac4bfa
Megan Olson <megan@>
2017-08-11 14:41:21 -0700

    Add final styling components

--- js/scripts.js
+++ js/scripts.js
@@ -17,12 +17,15 @@ $(document).ready(function() {
       if (answer1 === 'small companies and startups' && answer2 === 'yes - i love it' || answer2 === "no - I wouldn't know where to start" && answer3 === 'creating a formula to act with user input' && answer4 === 'no - I am persistent and determined but I like to see my project up and running with faster results' && answer5 === 'not neccesarily') {
         $('.ruby').show();
         $('.nameOutput').text(nameInput);
+
       } else if (answer1 === 'large corporations' && answer2 === 'yes - i love it' || answer2 === "no - I wouldn't know where to start" && answer3 === 'writing elaborate software for a big database' && answer4 === 'yes - whatever it takes!' && answer5 ==='definitely') {
         $('.c').show();
         $('.nameOutput').text(nameInput);
+
       } else if (answer1 === 'large corporations' || answer1 === 'small companies and startups' && answer2 === 'yes - i love it' && answer3 === 'formatting a webpage according to user preference and usability' && answer4 === 'no - I am persistent and determined but I like to see my project up and running with faster results' || answer4 === 'yes - whatever it takes!' && answer5 ==='not neccesarily' || answer5 === 'definitely') {
         $('.css').show();
         $('.nameOutput').text(nameInput);
+      
       } else {
         $('.leftover').show();
         $('.nameOutput').text(nameInput);
